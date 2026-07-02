fdda7d622659d449b6236044cf9e84e7e1fda0c7
Muhammad Naqib <m.naqib.bin.m.najib@gmail.com>
2026-07-02 15:02:02 +0800

    ping prismlauncher to versions 9.x

--- modules/client.nix
+++ modules/client.nix
@@ -3,14 +3,27 @@
   nixpkgs.overlays = [
     (final: prev: {
       prismlauncher-custom = prev.prismlauncher.overrideAttrs (oldAttrs: rec {
-        version = "9.4";
+
+	# Metadata version name for my local package
+        #version = "9.4";
+        version = "9.x-branch;
+
         src = prev.fetchFromGitHub {
           owner = "PrismLauncher";
           repo = "PrismLauncher";
-          rev = version;
+
+          # Target the exact branch string
+          #rev = version;
+          rev = "release-9.x";
+
           fetchSubmodules = true;
-          hash = "sha256-Ndt0op00byJL2Xk2oJIMEwu8uVv0PTL9mHDk8kH3r/c=";
+          #hash = "sha256-Ndt0op00byJL2Xk2oJIMEwu8uVv0PTL9mHDk8kH3r/c=";
+          #hash = "";
+	  # Use a zero/fake hash. Nix will fail on rebuild, 
+          # inspect the download, and print out the correct hash for you to copy.
+          hash = lib.fakeHash;
         };
+
       });
     })
   ];
