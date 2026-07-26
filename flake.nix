21f5db4eef3ad780d57ad329e4f07a1bb607c819
M. Naqib Bin M. Najib <naqib@sumayah>
2026-07-26 10:06:43 +0800

    add syntax and option type test

--- flake.nix
+++ flake.nix
@@ -66,5 +66,90 @@
         '';
       };
 
+      #-----------------------------------------------
+      # Flake Checks for Automated Testing
+      #-----------------------------------------------
+      #
+      # To run all checks in parallel:
+      #   nix flake check
+      #
+      # To run check on devShells build
+      #   nix build .#checks.x86_64-linux.devShell-build
+      #
+      # To run test on nixosModules.minecraft-server wheater it evaluates porperly without syntax or option type errors.
+      #   nix build .#checks.x86_64-linux.server-module-eval --no-link
+      # Note: The --no-link flag tells Nix to evaluate and build the check result without creating a ./result symlink in your working directory.
+      #
+      # To run test on homeModules.minecraft-client
+      #   nix build .#checks.x86_64-linux.client-module-eval --no-link
+      # Note: The --no-link flag tells Nix to evaluate and build the check result without creating a ./result symlink in your working directory.
+      #
+      # To run test vm
+      #   nix build .#checks.x86_64-linux.server-vm-test
+      #
+      checks.${system} = {
+
+        # Test that the default devShell builds and contains packwiz
+        devShell-build = self.devShells.${system}.default;
+
+        /*
+
+        # Test evaluation of the NixOS module configuration
+        server-module-eval = (nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs;  };
+          modules = [
+            self.nixosModules.minecraft-server
+            {
+              boot.loader.grub.device = "nodev";
+              fileSystems."/" = {
+                device = "/dev/null";
+                fsType = "ext4";
+              };
+            }
+          ];
+        }).config.system.build.toplevel;
+
+        # Test evaluation of the Home Manager Client Module
+        client-module-eval = (home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs;  };
+          modules = [
+            self.homeModules.minecraft-client
+            {
+              # Minimal required boilerplate for Home Manager evaluation
+              home.username = "testuser";
+              home.homeDirectory = "/home/testuser";
+              home.stateVersion = "24.05";
+            }
+          ];
+        }).activationPackage;
+
+        # Full VM Integration Test (boots a VM and checks if service runs)
+        server-vm-test = nixpkgs.lib.testers.runNixOSTest {
+          name = "minecraft-server-startup-test";
+          node = {
+            specialArgs = { inherit inputs; };
+          };
+          nodes.server = { pkgs, ... }: {
+            imports = [ self.nixosModules.minecraft-server ];
+
+            # Basic dummy configuration to allow the test VM to boot
+            services.minecraft-servers = {
+              enable = true;
+              eula = true;
+            };
+          };
+
+          testScript = ''
+            server.wait_for_unit("multi-user.target")
+            server.succeed("systemctl is-enabled minecraft-servers.target || true")
+          '';
+        };
+
+        */
+
+      };
+
     };
 }
